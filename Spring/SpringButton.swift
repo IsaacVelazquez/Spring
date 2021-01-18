48b739da35df04a961d6ab615fa265dbda394316
Isaac Velazquez <ivazquezn@gbm.com.mx>
2021-01-18 11:51:10 -0600

    Add @objc to classes

--- Spring/SpringButton.swift
+++ Spring/SpringButton.swift
@@ -22,7 +22,7 @@
 
 import UIKit
 
-open class SpringButton: UIButton, Springable {
+@objc open class SpringButton: UIButton, Springable {
     @IBInspectable public var autostart: Bool = false
     @IBInspectable public var autohide: Bool = false
     @IBInspectable public var animation: String = ""
